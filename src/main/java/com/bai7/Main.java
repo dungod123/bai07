51f8453d73a92d2538aaafb5a9693180b2d30503
hoang dung <dunghoangvolao123@gmail.com>
2026-05-02 22:54:29 +0700

    a

--- src/main/java/com/bai7/Main.java
+++ src/main/java/com/bai7/Main.java
@@ -2,6 +2,7 @@ package com.bai7;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        public int fIrstPosiTive=2;
+        System.out.println(fIrstPosiTive);
     }
 }
