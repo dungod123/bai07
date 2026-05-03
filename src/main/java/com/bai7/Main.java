ee266d74566f0124829e92e66ff8538fd46299ba
hoang dung <dunghoangvolao123@gmail.com>
2026-05-03 21:17:06 +0700

    commit 3

--- src/main/java/com/bai7/Main.java
+++ src/main/java/com/bai7/Main.java
@@ -2,7 +2,7 @@ package com.bai7;
 
 public class Main {
     public static void main(String[] args) {
-        public int fIrstPosiTive=2;
+        int fIrstPosiTive=2;
         System.out.println(fIrstPosiTive);
     }
 }
