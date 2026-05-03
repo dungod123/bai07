e9cfe5ea2bfacfd5b6f6ebf77cbecb32528482b1
hoang dung <dunghoangvolao123@gmail.com>
2026-05-03 21:19:55 +0700

    dev4

--- src/main/java/com/bai7/Main.java
+++ src/main/java/com/bai7/Main.java
@@ -2,7 +2,8 @@ package com.bai7;
 
 public class Main {
     public static void main(String[] args) {
-        int fIrstPosiTive=2;
-        System.out.println(fIrstPosiTive);
+        int FIRSstPosiTive=2;
+        int Mynumber=2;
+        System.out.println(FIRSstPosiTive+Mynumber);
     }
 }
